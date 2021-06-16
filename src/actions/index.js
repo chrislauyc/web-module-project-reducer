d408de5ebf032cea0386cf7a458a1ce5e9ec76ab
Chris Lau <chrislyc1991@gmail.com>
2021-06-15 19:36:15 -0600

    finished mvp and stretch with adding digits

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,11 +2,33 @@ export const ADD_ONE = "ADD_ONE";
 
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const CLEAR_DISPLAY = "CLEAR_DISPLAY";
+export const MEMORY_ADD = "MEMORY_ADD";
+export const MEMORY_RECALL = "MEMORY_RECALL";
+export const MEMORY_CLEAR = "MEMORY_CLEAR";
+export const ADD_DIGIT = "ADD_DIGIT";
 
 export const addOne = () => {
     return({type:ADD_ONE});
 }
-
 export const applyNumber = (number) => {
     return({type:APPLY_NUMBER, payload:number});
 }
+export const changeOperation = (operation) =>{
+    return({type:CHANGE_OPERATION, payload:operation});
+}
+export const clearDisplay = () =>{
+    return({type:CLEAR_DISPLAY});
+}
+export const memoryAdd = () =>{
+    return({type:MEMORY_ADD});
+}
+export const memoryRecall = () =>{
+    return({type:MEMORY_RECALL});
+}
+export const memoryClear = () =>{
+    return({type:MEMORY_CLEAR});
+}
+export const addDigit = (digit) =>{
+    return({type:ADD_DIGIT,payload:digit});
+} 
